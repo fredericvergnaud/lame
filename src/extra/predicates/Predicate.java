34274d5b56ebcf618a8b65ee5d7e92183394aca0
Frédéric Vergnaud <frederic.vergnaud@mines-paristech.fr>
2019-10-03 15:56:09 +0200

    Remove src

--- src/extra/predicates/Predicate.java
+++
@@ -1,5 +0,0 @@
-package extra.predicates;
-
-public interface Predicate<T> {
-	public boolean eval(T value);
-}
--- src/modeles/ecouteurs/ListeListener.java
+++
@@ -1,9 +0,0 @@
-package modeles.ecouteurs;
-
-import java.util.EventListener;
-
-import modeles.evenements.ListeChangedEvent;
-
-public interface ListeListener extends EventListener {
-	public void listeChanged(ListeChangedEvent event);
-}
